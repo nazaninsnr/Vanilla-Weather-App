4ba72b15cebb6554b34f9f98a3189e8e7f7451c1
nazaninsnr <103143082+nazaninsnr@users.noreply.github.com>
2022-04-28 12:11:44 +0430

    Updated the time

--- src/script.js
+++ src/script.js
@@ -1,13 +1,40 @@
-function  displayTemperature(response) {
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description")
-    let temperatureElement = document.querySelector("#temperature");
-    cityElement.innerHTML = response.data.name;
-    descriptionElement.innerHTML = response.data.weather[0].description;
-    temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  let dateElement = document.querySelector("#date");
+  let descriptionElement = document.querySelector("#description");
+  let temperatureElement = document.querySelector("#temperature");
+
+  cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}°`;
 }
 
 let apiKey = "506d09fcd6d63e6d24952f9fd229a34e";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tehran&appid=${apiKey}&units=metric`;
+let city = "Tehran";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
